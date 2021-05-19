fd38f8b63f60f7f9f935970267e01a1b2a17e7e6
Matheusmdr <matheus.m.rocha@unesp.br>
2021-05-19 01:21:26 -0300

    Add Bootstrap and JQuery

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    $toggleCollapse.click(function () {
+      $nav.toggleClass('collapse');
+    })
+  });
