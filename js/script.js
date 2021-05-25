acad326565e466aebcb3b258ef8e732fd0c891aa
Matheusmdr <matheus.m.rocha@unesp.br>
2021-05-25 01:18:29 -0300

    add go-top arrow in home

--- js/script.js
+++ js/script.js
@@ -11,6 +11,18 @@ $(document).ready(function () {
 
   });
 
+  const gotoTop = document.querySelector(".goto-top");
+  
+  window.addEventListener("scroll", e => {
+    const scrollHeight = window.pageYOffset;
+ 
+    if (scrollHeight > 300) {
+      gotoTop.classList.add("show-top");
+    } else {
+      gotoTop.classList.remove("show-top");
+    }
+  });
+
 
   const slider1 = document.getElementById("glide_1");
 
@@ -26,3 +38,5 @@ $(document).ready(function () {
       animationTimingFunc: "linear",
     }).mount();
   }
+
+
