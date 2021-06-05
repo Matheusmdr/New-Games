b3a4740308eda05e7994d1334578d878873d7531
Matheusmdr <matheus.m.rocha@unesp.br>
2021-06-05 01:33:46 -0300

    Cart changes

--- js/cartNum.js
+++ js/cartNum.js
@@ -1,18 +1,10 @@
 function CartUp(){
-    if (sessionStorage['quantProds']) {
-        let prods = JSON.parse(sessionStorage.getItem('quantProds'));
+    if (sessionStorage['somaQuant']) {
+        let prods = JSON.parse(sessionStorage.getItem('somaQuant'));
         let cartUp = document.getElementById("cart-total");
         if (prods > 0) {
             cartUp.style.display = "block";
             cartUp.innerHTML = `${prods}`;
         } else cartUp.style.display = "none";
     }
-}
-
-window.onstorage = () => {
-    CartUp();
-};
-
-window.onload = () => {
-    CartUp();
 }
